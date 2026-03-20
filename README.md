6c3e930e5b87097645b15570b107fcd6ab8cbec2
Sayan Ray <ray.sayan27@gmail.com>
2026-03-20 13:12:20 +0530

    Add Word Counter task

--- README.md
+++ README.md
@@ -10,3 +10,17 @@ go run main.go
 ## Run with Docker
 docker build -t number-analyzer .
 docker run -it number-analyzer
+
+
+# Word Counter (Go + Docker)
+
+## Features
+- Count total words
+- Count frequency of each word
+
+## Run Locally
+go run main.go
+
+## Run with Docker
+docker build -t word-counter .
+docker run -it word-counter
--- main.go
+++ main.go
@@ -1,39 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
 
-	fmt.Println("Number analyzer task - Odd/Even/Prime")
-	var num int
+	// fmt.Println("Number analyzer task - Odd/Even/Prime")
+	// var num int
 
-	fmt.Print("Enter a number: ")
-	fmt.Scan(&num)
+	// fmt.Print("Enter a number: ")
+	// fmt.Scan(&num)
 
-	// Even or Odd
-	if num%2 == 0 {
-		fmt.Print("Even, ")
-	} else {
-		fmt.Print("Odd, ")
+	// // Even or Odd
+	// if num%2 == 0 {
+	// 	fmt.Print("Even, ")
+	// } else {
+	// 	fmt.Print("Odd, ")
+	// }
+	// // Prime or Not
+	// if isPrime(num) {
+	// 	fmt.Println("Prime")
+	// } else {
+	// 	fmt.Println("Not Prime")
+	// }
+
+	///word count
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter a sentence: ")
+	input, _ := reader.ReadString('\n')
+
+	input = strings.ToLower(input)
+
+	words := strings.Fields(input)
+
+	wordCount := make(map[string]int)
+
+	for _, word := range words {
+		wordCount[word]++
 	}
 
-	// Prime or Not
-	if isPrime(num) {
-		fmt.Println("Prime")
-	} else {
-		fmt.Println("Not Prime")
+	fmt.Println("\nWord Frequency:")
+	for word, count := range wordCount {
+		fmt.Printf("%s: %d\n", word, count)
 	}
+
 }
 
 // function to check prime
-func isPrime(n int) bool {
-	if n <= 1 {
-		return false
-	}
-	for i := 2; i*i <= n; i++ {
-		if n%i == 0 {
-			return false
-		}
-	}
-	return true
-}
+// func isPrime(n int) bool {
+// 	if n <= 1 {
+// 		return false
+// 	}
+// 	for i := 2; i*i <= n; i++ {
+// 		if n%i == 0 {
+// 			return false
+// 		}
+// 	}
+// 	return true
+// }
